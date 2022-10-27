282af2d3bfbaccf0796255e3509107db7695656d
TaeWoonJeong <pig07911@naver.com>
2022-10-27 16:47:12 +0900

    init multi-module project

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.0" apply false
+    id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21" apply false
+}
+
+allprojects {
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+    dependencies {
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.getByName("bootJar") {
+        enabled=true
+    }
+
+    tasks.getByName("jar") {
+        enabled=true
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ module2/src/main/kotlin/com/example/module2/controller/Controller.kt
@@ -0,0 +1,16 @@
+package com.example.module2.controller
+
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+
+@RestController
+class Controller(private val environment: Environment) {
+    @GetMapping("/")
+    fun get():String {
+        var port: String? = environment.getProperty("local.server.port")
+        return "module2의 현재 시간은 "+LocalDateTime.now().toString() + "이고, 포트는 $port 입니다."
+    }
+}
