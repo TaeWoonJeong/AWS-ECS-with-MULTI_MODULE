ad274799ff40490a4811acbe69dc35f072daca14
TaeWoonJeong <pig07911@naver.com>
2022-10-30 20:08:33 +0900

    feat: full ci/cd try5

--- module1/src/main/kotlin/com/example/module1/controller/Controller.kt
+++ module1/src/main/kotlin/com/example/module1/controller/Controller.kt
@@ -4,13 +4,15 @@ import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
+import java.util.UUID
 
 
 @RestController
 class Controller(private val environment: Environment) {
+    private val uuid = UUID.randomUUID()
     @GetMapping("/")
     fun get():String {
         val port: String? = environment.getProperty("local.server.port")
-        return "module1의 현재 시간은 "+LocalDateTime.now().toString() + "이고, 포트는 $port 입니다."
+        return "module2의 현재 시간은 "+LocalDateTime.now().toString() + "이고, 포트는 $port 입니다. uuid 값 : $uuid"
     }
 }
