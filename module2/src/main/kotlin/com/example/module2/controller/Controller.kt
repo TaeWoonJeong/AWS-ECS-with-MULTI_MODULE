e20dd36f42e6c0bdeaa2d6cbf856a236349aa68e
TaeWoonJeong <pig07911@naver.com>
2022-11-03 19:39:25 +0900

    feat: test rolling update3

--- module2/src/main/kotlin/com/example/module2/controller/Controller.kt
+++ module2/src/main/kotlin/com/example/module2/controller/Controller.kt
@@ -16,8 +16,8 @@ class Controller(private val environment: Environment) {
         return "module2의 현재 시간은 "+LocalDateTime.now().toString() + "이고, 포트는 $port 입니다. uuid 값 : $uuid"
     }
 
-//    @GetMapping("/rolling")
-//    fun rolling() : String {
-//        return "module2 Rolling Update Completed!"
-//    }
+    @GetMapping("/rolling")
+    fun rolling() : String {
+        return "module2 Rolling Update Completed!"
+    }
 }
